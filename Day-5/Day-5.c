c6092944266226ca7b3404c41461d6491660c7c1
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-05 12:19:33 +0530

    Update Day-5.c

--- Day-5/Day-5.c
+++ Day-5/Day-5.c
@@ -1,1 +1,7 @@
-
+/*
+A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
+Input: - First line: integer p (number of entries in server log 1) 
+Second line: p sorted integers representing arrival times from server 1  
+Third line: integer q (number of entries in server log 2) 
+Fourth line: q sorted integers representing arrival times from server 2)
+*/
