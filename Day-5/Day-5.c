a9f05db944c9916c1f689a7d99cfc93fd2da3357
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-05 12:20:22 +0530

    Update Day-5.c

--- Day-5/Day-5.c
+++ Day-5/Day-5.c
@@ -5,3 +5,58 @@ Second line: p sorted integers representing arrival times from server 1
 Third line: integer q (number of entries in server log 2) 
 Fourth line: q sorted integers representing arrival times from server 2)
 */
+#include <stdio.h>
+
+int main() {
+    int p, q;
+    
+    // Input size of first log
+    scanf("%d", &p);
+    int A[p];
+    for(int i = 0; i < p; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    // Input size of second log
+    scanf("%d", &q);
+    int B[q];
+    for(int i = 0; i < q; i++) {
+        scanf("%d", &B[i]);
+    }
+
+    int C[p + q];
+    int i = 0, j = 0, k = 0;
+
+    // Merge both arrays
+    while(i < p && j < q) {
+        if(A[i] < B[j]) {
+            C[k] = A[i];
+            i++;
+        } else {
+            C[k] = B[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements of A
+    while(i < p) {
+        C[k] = A[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements of B
+    while(j < q) {
+        C[k] = B[j];
+        j++;
+        k++;
+    }
+
+    // Print merged array
+    for(int i = 0; i < p + q; i++) {
+        printf("%d ", C[i]);
+    }
+
+    return 0;
+}
