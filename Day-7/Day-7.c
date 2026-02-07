820b6b1d71bfd2389a0285e80f7ad1a5b0f0bdd2
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-07 14:26:29 +0530

    Update Day-7.c

--- Day-7/Day-7.c
+++ Day-7/Day-7.c
@@ -2,7 +2,6 @@
 Problem: Write a recursive function fib(n) to compute the n-th Fibonacci number where fib(0)=0 and fib(1)=1.
 */
 #include <stdio.h>
-
 int fib(int n)
 {
     if (n == 0)
@@ -12,13 +11,11 @@ int fib(int n)
     else
         return fib(n - 1) + fib(n - 2);
 }
-
 int main()
 {
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-
     printf("Fibonacci number is: %d", fib(n));
     return 0;
 }
