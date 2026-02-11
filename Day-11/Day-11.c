db90bc51276da4ed044edd692acd26787ebf8944
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-11 08:22:13 +0530

    Update Day-11.c

--- Day-11/Day-11.c
+++ Day-11/Day-11.c
@@ -1,1 +1,3 @@
-
+/*
+Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+*/
