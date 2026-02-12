b0a36b36c04c33e293cfc076c3c24932b6c0697f
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-12 18:46:23 +0530

    Add problem statement for matrix symmetry check
    Added a comment explaining the problem of checking matrix symmetry.

--- Day-12/Day-12.c
+++ Day-12/Day-12.c
@@ -1,1 +1,5 @@
-
+/*
+Problem: Write a program to check whether a given matrix is symmetric. A matrix is said to 
+be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position 
+[i][j] is equal to element at position [j][i] for all valid i and j).
+*/
