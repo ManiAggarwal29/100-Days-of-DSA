1b7f8145f3c9cda57e772598a4e96a08c0dfc5a7
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-13 23:44:59 +0530

    Implement clockwise spiral traversal of matrix

--- Day-13/Day-13.c
+++ Day-13/Day-13.c
@@ -2,3 +2,53 @@
 Problem: You are given a rectangular matrix of integers. Starting from the outer boundary, traverse the matrix in a clockwise manner and continue moving inward layer by layer
 until all elements are visited.
 */
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10];
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int top = 0, bottom = rows - 1;
+    int left = 0, right = cols - 1;
+
+    printf("Spiral Order Traversal:\n");
+
+    while(top <= bottom && left <= right) {
+
+        // Traverse from left to right
+        for(int i = left; i <= right; i++)
+            printf("%d ", matrix[top][i]);
+        top++;
+
+        // Traverse from top to bottom
+        for(int i = top; i <= bottom; i++)
+            printf("%d ", matrix[i][right]);
+        right--;
+
+        // Traverse from right to left
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--)
+                printf("%d ", matrix[bottom][i]);
+            bottom--;
+        }
+
+        // Traverse from bottom to top
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--)
+                printf("%d ", matrix[i][left]);
+            left++;
+        }
+    }
+
+    return 0;
+}
