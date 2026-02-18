a51b4b76d7debfb16def496906527c324f397cfc
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-18 22:29:39 +0530

    Refactor array rotation implementation

--- Day-18/Day-18.c
+++ Day-18/Day-18.c
@@ -2,39 +2,29 @@
 Problem: Given an array of integers, rotate the array to the right by k positions.
 */
 #include <stdio.h>
-
 int main() {
     int n, k, i, j, temp;
-    
     printf("Enter size of array: ");
     scanf("%d", &n);
-    
     int arr[n];
-    
     printf("Enter elements:\n");
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    
     printf("Enter k: ");
     scanf("%d", &k);
-    
     // Rotation
     for(i = 0; i < k; i++) {
         temp = arr[n-1];
-        
         for(j = n-1; j > 0; j--) {
             arr[j] = arr[j-1];
         }
-        
         arr[0] = temp;
     }
-    
     printf("Rotated array:\n");
     for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-    
     return 0;
 }
 
