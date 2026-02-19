2c75004211093ee8592f05e937d329d15321ae39
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-19 23:15:13 +0530

    Update Day-19.c

--- Day-19/Day-19.c
+++ Day-19/Day-19.c
@@ -3,26 +3,20 @@ Problem: Given an array of integers, find two elements whose sum is closest to z
 */
 #include <stdio.h>
 #include <stdlib.h>   // for abs()
-
 int main() {
     int n;
     scanf("%d", &n);
-
     int arr[n];
-    
     // input array
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
     int min_sum = 1000000;   // large value
     int x, y;
-
     // check all pairs
     for(int i = 0; i < n-1; i++) {
         for(int j = i+1; j < n; j++) {
             int sum = arr[i] + arr[j];
-
             if(abs(sum) < abs(min_sum)) {
                 min_sum = sum;
                 x = arr[i];
@@ -30,9 +24,7 @@ int main() {
             }
         }
     }
-
     // output result
     printf("%d %d", x, y);
-
     return 0;
 }
