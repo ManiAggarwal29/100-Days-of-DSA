d1ec24ff50097c9cd62b2825c5e775eca6d3b518
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-19 23:14:41 +0530

    Add closest sum to zero algorithm in Day-19.c
    Implement a program to find two elements in an array whose sum is closest to zero.

--- Day-19/Day-19.c
+++ Day-19/Day-19.c
@@ -1,3 +1,38 @@
 /*
 Problem: Given an array of integers, find two elements whose sum is closest to zero.
 */
+#include <stdio.h>
+#include <stdlib.h>   // for abs()
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    
+    // input array
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min_sum = 1000000;   // large value
+    int x, y;
+
+    // check all pairs
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            int sum = arr[i] + arr[j];
+
+            if(abs(sum) < abs(min_sum)) {
+                min_sum = sum;
+                x = arr[i];
+                y = arr[j];
+            }
+        }
+    }
+
+    // output result
+    printf("%d %d", x, y);
+
+    return 0;
+}
