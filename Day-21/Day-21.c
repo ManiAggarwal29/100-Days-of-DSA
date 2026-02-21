f89ee67e7cba01e961a0590a9a0f9de5fc297acd
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-21 10:35:16 +0530

    Implement singly linked list creation and traversal
    This program creates a singly linked list based on user input and traverses it to display the elements.

--- Day-21/Day-21.c
+++ Day-21/Day-21.c
@@ -1,3 +1,50 @@
 /*
 Problem: Create and Traverse Singly Linked List
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure of node
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *temp = NULL, *newnode;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    // creating linked list
+    for(i = 0; i < n; i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+
+        newnode->next = NULL;
+
+        if(head == NULL) {
+            head = newnode;
+            temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    // traversing linked list
+    printf("Linked List Elements: ");
+    temp = head;
+
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
