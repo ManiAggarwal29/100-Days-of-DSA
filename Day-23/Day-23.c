fa905ae6553f844a05348e49301765524a8bf9bf
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-23 07:21:45 +0530

    Update Day-23.c

--- Day-23/Day-23.c
+++ Day-23/Day-23.c
@@ -1,3 +1,96 @@
 /*
 Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct node {
+    int data;
+    struct node* next;
+};
+
+// Create new node
+struct node* createNode(int value) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct node* insert(struct node* head, int value) {
+    struct node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    struct node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Merge two sorted lists
+struct node* merge(struct node* l1, struct node* l2) {
+    struct node dummy;
+    struct node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    // Attach remaining nodes
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+// Print list
+void display(struct node* head) {
+    struct node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m, x;
+    struct node *l1 = NULL, *l2 = NULL;
+
+    // First list
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        l1 = insert(l1, x);
+    }
+
+    // Second list
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &x);
+        l2 = insert(l2, x);
+    }
+
+    // Merge lists
+    struct node* result = merge(l1, l2);
+
+    // Output
+    display(result);
+
+    return 0;
+}
