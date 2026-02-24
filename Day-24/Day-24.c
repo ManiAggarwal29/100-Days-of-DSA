cfc08b140fdcef35764a1a295090c36bed29049e
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-24 22:13:08 +0530

    Implement delete first occurrence of a key in linked list

--- Day-24/Day-24.c
+++ Day-24/Day-24.c
@@ -3,59 +3,47 @@ Problem: Delete First Occurrence of a Key - Implement using linked list with dyn
 */
 #include <stdio.h>
 #include <stdlib.h>
-
 struct node {
     int data;
     struct node* next;
 };
-
 // Function to delete first occurrence of key
 struct node* deleteKey(struct node* head, int key) {
     struct node *temp = head, *prev = NULL;
-
     // If head node itself holds the key
     if (temp != NULL && temp->data == key) {
         head = temp->next;
         free(temp);
         return head;
     }
-
     // Search for the key
     while (temp != NULL && temp->data != key) {
         prev = temp;
         temp = temp->next;
     }
-
     // If key not found
     if (temp == NULL) {
         printf("Key not found\n");
         return head;
     }
-
     // Unlink the node
     prev->next = temp->next;
     free(temp);
-
     return head;
 }
-
 // Function to insert at end
 struct node* insert(struct node* head, int data) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
     newNode->data = data;
     newNode->next = NULL;
-
     if (head == NULL)
         return newNode;
-
     struct node* temp = head;
     while (temp->next != NULL)
         temp = temp->next;
-
     temp->next = newNode;
     return head;
 }
-
 // Display list
 void display(struct node* head) {
     struct node* temp = head;
@@ -65,24 +53,18 @@ void display(struct node* head) {
     }
     printf("NULL\n");
 }
-
 int main() {
     struct node* head = NULL;
-
     // Creating list
     head = insert(head, 10);
     head = insert(head, 20);
     head = insert(head, 30);
     head = insert(head, 20);
-
     printf("Original List:\n");
     display(head);
-
     int key = 20;
     head = deleteKey(head, key);
-
     printf("After Deleting %d:\n", key);
     display(head);
-
     return 0;
 }
