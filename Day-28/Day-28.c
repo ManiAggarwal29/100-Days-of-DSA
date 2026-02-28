52982616dae840a340ae478894e00bb40f246cf9
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-02-28 23:52:29 +0530

    Implement circular linked list creation and traversal

--- Day-28/Day-28.c
+++ Day-28/Day-28.c
@@ -3,27 +3,21 @@ Problem: Circular Linked List Creation and Traversal - Implement using linked li
 */
 #include <stdio.h>
 #include <stdlib.h>
-
 // Structure of node
 struct node {
     int data;
     struct node *next;
 };
-
 // Function to create circular linked list
 struct node* createList(int n) {
     struct node *head = NULL, *temp, *newnode;
     int i;
-
     for(i = 1; i <= n; i++) {
         // Allocate memory
         newnode = (struct node*)malloc(sizeof(struct node));
-
         printf("Enter data for node %d: ", i);
         scanf("%d", &newnode->data);
-
         newnode->next = NULL;
-
         if(head == NULL) {
             head = newnode;
             temp = newnode;
@@ -33,45 +27,32 @@ struct node* createList(int n) {
             temp = newnode;
         }
     }
-
     // Make it circular
     temp->next = head;
-
     return head;
 }
-
 // Function to traverse circular linked list
 void traverse(struct node *head) {
     struct node *temp;
-
     if(head == NULL) {
         printf("List is empty\n");
         return;
     }
-
     temp = head;
-
     printf("Circular Linked List: ");
-
     do {
         printf("%d -> ", temp->data);
         temp = temp->next;
     } while(temp != head);
-
     printf("(back to head)\n");
 }
-
 // Main function
 int main() {
     struct node *head;
     int n;
-
     printf("Enter number of nodes: ");
     scanf("%d", &n);
-
     head = createList(n);
-
     traverse(head);
-
     return 0;
 }
