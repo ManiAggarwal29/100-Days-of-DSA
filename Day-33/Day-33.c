10116323bb53dae3f62c0d0a64678ebb3a940618
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-03-05 23:06:27 +0530

    Update Day-33.c

--- Day-33/Day-33.c
+++ Day-33/Day-33.c
@@ -4,49 +4,38 @@ Problem: Convert an infix expression to postfix notation using stack.
 #include <stdio.h>
 #include <ctype.h>
 #define MAX 100
-
 char stack[MAX];
 int top = -1;
-
 void push(char x){
     stack[++top] = x;
 }
-
 char pop(){
     return stack[top--];
 }
-
 int precedence(char x){
     if(x=='+' || x=='-') return 1;
     if(x=='*' || x=='/') return 2;
     if(x=='^') return 3;
     return 0;
 }
-
 int main(){
     char infix[100], postfix[100];
     int i=0, j=0;
-    
     printf("Enter Infix Expression: ");
     scanf("%s", infix);
-
     while(infix[i] != '\0'){
-        
         if(isalnum(infix[i])){
             postfix[j++] = infix[i];
         }
-        
         else if(infix[i] == '('){
             push(infix[i]);
         }
-        
         else if(infix[i] == ')'){
             while(stack[top] != '('){
                 postfix[j++] = pop();
             }
             pop();
         }
-        
         else{
             while(top != -1 && precedence(stack[top]) >= precedence(infix[i])){
                 postfix[j++] = pop();
@@ -55,14 +44,10 @@ int main(){
         }
         i++;
     }
-
     while(top != -1){
         postfix[j++] = pop();
     }
-
     postfix[j] = '\0';
-
     printf("Postfix Expression: %s", postfix);
-
     return 0;
 }
