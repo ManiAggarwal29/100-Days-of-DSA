e8f911dcfa020a5c07a31151fe300c8097d914b8
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-03-10 23:03:04 +0530

    Update Day-38.c

--- Day-38/Day-38.c
+++ Day-38/Day-38.c
@@ -1,1 +1,5 @@
-
+/*
+Problem: Deque (Double-Ended Queue)
+A Deque is a linear data structure that allows insertion and deletion of elements from both the front and the rear. It provides more 
+flexibility than a standard queue or stack.
+*/
