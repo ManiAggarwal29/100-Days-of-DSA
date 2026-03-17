c6b8d7881545f2370d5393cac2ef36a77348befe
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-03-17 22:20:13 +0530

    Update Day-45.c

--- Day-45/Day-45.c
+++ Day-45/Day-45.c
@@ -3,43 +3,33 @@ Find the height (maximum depth) of a given binary tree.
 */
 #include <stdio.h>
 #include <stdlib.h>
-
 struct node {
     int data;
     struct node* left;
     struct node* right;
 };
-
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
-
 int height(struct node* root) {
     if (root == NULL)
         return 0;
-
     int leftHeight = height(root->left);
     int rightHeight = height(root->right);
-
     return 1 + max(leftHeight, rightHeight);
 }
-
 struct node* newNode(int data) {
     struct node* temp = (struct node*)malloc(sizeof(struct node));
     temp->data = data;
     temp->left = temp->right = NULL;
     return temp;
 }
-
 int main() {
-
     struct node* root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(3);
     root->left->left = newNode(4);
     root->left->right = newNode(5);
-
     printf("Height of tree: %d", height(root));
-
     return 0;
 }
