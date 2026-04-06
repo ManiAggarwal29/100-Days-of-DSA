7148be9163e5303401ad4f9e79d8d69661647c8a
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-06 21:26:14 +0530

    Add cycle detection in undirected graph using DFS
    Implemented DFS algorithm to detect cycles in an undirected graph using an adjacency matrix.

--- Day-65/Day-65.c
+++ Day-65/Day-65.c
@@ -1,3 +1,59 @@
 /*
 Problem: Using DFS and parent tracking, detect if undirected graph has a cycle.
 */
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+int graph[MAX][MAX]; // adjacency matrix
+bool visited[MAX];
+int n; // number of vertices
+
+bool dfs(int node, int parent) {
+    visited[node] = true;
+
+    for (int i = 0; i < n; i++) {
+        if (graph[node][i]) { // if edge exists
+            if (!visited[i]) {
+                if (dfs(i, node))
+                    return true;
+            }
+            else if (i != parent) {
+                return true; // cycle found
+            }
+        }
+    }
+    return false;
+}
+
+bool hasCycle() {
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i, -1))
+                return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    n = 4;
+
+    // Example graph
+    // 0-1, 1-2, 2-0 forms a cycle
+    graph[0][1] = graph[1][0] = 1;
+    graph[1][2] = graph[2][1] = 1;
+    graph[2][0] = graph[0][2] = 1;
+
+    if (hasCycle())
+        printf("Cycle exists\n");
+    else
+        printf("No cycle\n");
+
+    return 0;
+}
