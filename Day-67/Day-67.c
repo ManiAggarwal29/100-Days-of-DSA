edc110ef1afa186c6b7e3a8a3ed9072c9c04a9fa
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-08 14:13:17 +0530

    Implement topological sorting for a DAG using DFS

--- Day-67/Day-67.c
+++ Day-67/Day-67.c
@@ -3,29 +3,23 @@ Problem: Print topological ordering of a Directed Acyclic Graph (DAG) using DFS.
 */
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX 100
-
 int graph[MAX][MAX];
 int visited[MAX];
 int stack[MAX];
 int top = -1;
 int n;
-
 // DFS function
 void dfs(int v) {
     visited[v] = 1;
-
     for(int i = 0; i < n; i++) {
         if(graph[v][i] == 1 && visited[i] == 0) {
             dfs(i);
         }
     }
-
     // push into stack
     stack[++top] = v;
 }
-
 // Topological Sort function
 void topologicalSort() {
     for(int i = 0; i < n; i++) {
@@ -33,30 +27,24 @@ void topologicalSort() {
             dfs(i);
         }
     }
-
     printf("Topological Order:\n");
     while(top != -1) {
         printf("%d ", stack[top--]);
     }
 }
-
 int main() {
     printf("Enter number of vertices: ");
     scanf("%d", &n);
-
     printf("Enter adjacency matrix:\n");
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             scanf("%d", &graph[i][j]);
         }
     }
-
     // initialize visited array
     for(int i = 0; i < n; i++) {
         visited[i] = 0;
     }
-
     topologicalSort();
-
     return 0;
 }
