e416403bb6c9aa59faf91ebeb989abdee11b0601
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-13 23:05:46 +0530

    Add problem statement comment for repeated character
    Added a comment explaining the problem statement for finding the first repeated character in a string.

--- Day-72/Day-72.c
+++ Day-72/Day-72.c
@@ -1,1 +1,5 @@
-
+/*
+Given a string s consisting of lowercase letters, find the first repeated character in the string. A character is 
+considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has 
+the smallest index should be returned.
+*/
