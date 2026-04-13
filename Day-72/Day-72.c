376388c8e181432e4740aba88dc56f84fb992637
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-13 23:06:17 +0530

    Implement function to find first repeated character

--- Day-72/Day-72.c
+++ Day-72/Day-72.c
@@ -3,3 +3,36 @@ Given a string s consisting of lowercase letters, find the first repeated charac
 considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has 
 the smallest index should be returned.
 */
+#include <stdio.h>
+
+char firstRepeated(char s[]) {
+    int seen[26] = {0};   // for a to z
+    
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+        
+        if (seen[index] == 1) {
+            return s[i];   // first repeated character
+        }
+        
+        seen[index] = 1;
+    }
+    
+    return '\0';  // if no repeated character
+}
+
+int main() {
+    char s[100];
+    
+    printf("Enter string: ");
+    scanf("%s", s);
+    
+    char result = firstRepeated(s);
+    
+    if (result != '\0')
+        printf("First repeated character = %c", result);
+    else
+        printf("No repeated character");
+    
+    return 0;
+}
