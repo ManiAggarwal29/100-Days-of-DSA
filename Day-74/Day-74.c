51f582827dca4940f0e8d4c7bf70b3e58c5e0e0f
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-15 21:52:30 +0530

    Include problem statement in Day-74.c
    Add problem statement for vote counting task.

--- Day-74/Day-74.c
+++ Day-74/Day-74.c
@@ -1,1 +1,5 @@
-
+/*
+Problem Statement
+Given an array of candidate names where each name represents a vote cast for that candidate, determine the candidate 
+who received the maximum number of votes. In case of a tie, return the lexicographically smallest candidate name.
+*/
