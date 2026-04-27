5103a426a9421a4b525480d86326fa1bd836b545
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-27 22:19:44 +0530

    Update Day-86.c

--- Day-86/Day-86.c
+++ Day-86/Day-86.c
@@ -1,1 +1,4 @@
-
+/*
+Problem: Find the integer square root of a given non-negative integer using Binary Search. The integer square root of a
+number is defined as the greatest integer whose square is less than or equal to the given number.
+*/
