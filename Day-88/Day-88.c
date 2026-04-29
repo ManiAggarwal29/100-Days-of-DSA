15a8bd9663979b94fa45cf7a6e6214e67ff29eeb
ManiAggarwal29 <maniaggarwal29@gmail.com>
2026-04-29 14:14:12 +0530

    Update Day-88.c

--- Day-88/Day-88.c
+++ Day-88/Day-88.c
@@ -1,1 +1,5 @@
-
+/*
+Problem Statement
+Given n stalls located at different positions along a straight line and k cows, place the cows in the stalls such that the 
+minimum distance between any two cows is maximized.
+*/
